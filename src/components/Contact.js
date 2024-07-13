72e6f81425ccaffe955958c2e60c99882d60b6ed
vrajpatel1812 <vrajp771@gmail.com>
2024-07-13 14:52:53 +0530

    update about

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,91 @@
-import React from 'react';
-import './Contact.css';
+import React, { useRef, useState } from "react";
+import "./Contact.css";
+import emailjs from "@emailjs/browser";
+
+//service_58glton Service ID
+//template_qcmfxeo template ID
+//MkCling1126Rp1Ha- public ID
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(
+        "service_58glton",
+        "template_qcmfxeo",
+        {
+          from_name: form.name,
+          to_name: "Vraj",
+          from_email: form.email,
+          to_email: "vrajp771@gmail.com",
+          message: form.message,
+        },
+        "MkCling1126Rp1Ha-"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Mail Send");
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("Something went wrong.");
+        }
+      );
+    setLoading(true);
+  };
+
   return (
     <section id="contact" className="contact">
       <h2>Contact Us</h2>
       <p>Get in touch for a free consultation.</p>
-      <form>
+      <form ref={formRef} onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" required />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          required
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          required
+        />
 
         <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" required></textarea>
+        <textarea
+          id="message"
+          name="message"
+          value={form.message}
+          onChange={handleChange}
+          required
+        ></textarea>
 
-        <button type="submit">Send Message</button>
+        <button type="submit">{loading ? "Sending..." : "Send"}</button>
       </form>
     </section>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,19 @@ import "./Home.css";
 import my from "./my.jpeg";
 import smirati from "./smriti.png"; // Import your new image here
 import line from "./line2-1 copy.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
 
 const Home = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -10,10 +23,41 @@ const Home = () => {
   return (
     <div className="home">
       <div className="mycolor">
-        <p><br></br></p>
-        <p className="intro-text">
-          Whether it's hopping on a [virtual] velvet sofa together to chat about your vision or handing over the reins completely, we're passionate about meeting your design needs!
+        <p>
+          <br></br>
         </p>
+
+        <div className="hero-intro">
+          <div className="hero-text">
+            <p className="intro-text">
+              Whether it's hopping on a [virtual] velvet sofa together to chat
+              about your vision or handing over the reins completely, we're
+              passionate about meeting your design needs!
+            </p>
+          </div>
+          <div className="hero-image">
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+              spaceBetween={50}
+              slidesPerView={1}
+              pagination={{ clickable: true }}
+              // navigation          //for sidebar
+              // scrollbar={{ draggable: true }} //for scrollbar
+              autoplay={{ delay: 3000, disableOnInteraction: false }} // Enable autoplay
+              loop={true} // Enable loop
+            >
+              <SwiperSlide>
+                <img src={my} alt="my" className="intro-image" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={my} alt="my" className="intro-image" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src={my} alt="my" className="intro-image" />
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </div>
       </div>
       <div className="intro">
         <div className="parallax-container">
@@ -40,20 +84,24 @@ const Home = () => {
             <div className="title-and-paragraph">
               <h2 className="cursive-title">Design à la carte</h2>
               <p>
-                Wish you could just pick your designer's brain for an hour without fear of commitment? Not even sure where to begin in your home? Receive design solutions, styling tips, & more through our in-home or online consultations!
+                Wish you could just pick your designer's brain for an hour
+                without fear of commitment? Not even sure where to begin in your
+                home? Receive design solutions, styling tips, & more through our
+                in-home or online consultations!
               </p>
-             <br></br>
-              
+              <br></br>
+
               <p> starting at: $400/hour</p>
-              
-              
             </div>
           </div>
         </div>
         <div className="section even">
           <div className="section-content">
             <p>
-              Ready to finally make your space Pinterest-worthy with the help of a design professional? Choose between a virtual 3D makeover to jumpstart your DIY project or kick up your feet with our effortless start-to-finish makeovers!
+              Ready to finally make your space Pinterest-worthy with the help of
+              a design professional? Choose between a virtual 3D makeover to
+              jumpstart your DIY project or kick up your feet with our
+              effortless start-to-finish makeovers!
             </p>
             <img src={my} alt="Description1" />
           </div>
@@ -62,14 +110,20 @@ const Home = () => {
           <div className="section-content">
             <img src={my} alt="Description1" />
             <p>
-              On the hunt for that perfect kitchen backsplash? Going crazy staring at all those paint swatches on the wall? Be a little picky with your design needs thanks to our à la carte package before making costly mistakes in your home!
+              On the hunt for that perfect kitchen backsplash? Going crazy
+              staring at all those paint swatches on the wall? Be a little picky
+              with your design needs thanks to our à la carte package before
+              making costly mistakes in your home!
             </p>
           </div>
         </div>
         <div className="section even">
           <div className="section-content">
             <p>
-              On the hunt for that perfect kitchen backsplash? Going crazy staring at all those paint swatches on the wall? Be a little picky with your design needs thanks to our à la carte package before making costly mistakes in your home!
+              On the hunt for that perfect kitchen backsplash? Going crazy
+              staring at all those paint swatches on the wall? Be a little picky
+              with your design needs thanks to our à la carte package before
+              making costly mistakes in your home!
             </p>
             <img src={my} alt="Description1" />
           </div>
